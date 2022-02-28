d37ba3adf1055a62d00ea2506c4fb9999b90f424
Akam <akambilbas@gmail.com>
2022-02-28 11:12:38 +0100

    Making classes

--- input_reader.py
+++ input_reader.py
@@ -2,10 +2,12 @@
 import io
 import newick as nw
 
-with io.open('data/carnivora.newick', encoding='utf8') as file:
-    trees = nw.load(file)
+class InputReader:
 
-def read_child():
-    
-    
+    def __init__(self, path):
+        with io.open(path, encoding='utf8') as file:
+            self.trees = nw.load(file)[0]
+            print(self.trees.ascii_art())
 
+    def read_child():
+        pass
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Main file to run the visualization tool
+from input_reader import InputReader
+
+class Main:
+
+    def __init__(self, path):
+        self.in_reader = InputReader(path)
+
+
+Main('./data/carnivora.newick')
