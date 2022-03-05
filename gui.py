73fb9d16888dd118fa2fc427dbfa4ef6eda3e608
Pim van Leeuwen <pimvleeuwen@outlook.com>
2022-03-05 15:55:28 +0100

    Some layout changes

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ from input_reader import InputReader
 def import_button_function(self):
     path = filedialog.askopenfile(initialdir="/", title="Select file").name
     self.input_reader = InputReader(path[:len(path)-5])
+    self.file_label.config(text = path[:len(path)-5])
 
 def change_vis_button_function(self):
     if self.input_reader != None:
@@ -15,7 +16,7 @@ def change_vis_button_function(self):
         print("no file selected yet")
 
 def export_button_function(self):
-    print("not yet implemented")
+    print(self.import_button.winfo_width())
 
 class GUI:
 
@@ -29,52 +30,56 @@ class GUI:
 
     def run_gui(self):
         
-        
         # The root frame
-        root = Tk()
-        root.geometry("700x700")
-        root.configure(background=self.bg_color)
-        root.title("Circular Space Reclaiming Plots Visualizer")
-
-        # The grid layout
-        for i in range(5):
-            root.rowconfigure(i, weight=1)
-        for i in range(4):
-            root.columnconfigure(i, weight=1)
+        self.root = Tk()
+        self.root.geometry("700x700")
+        self.root.configure(background=self.bg_color)
+        self.root.title("Circular Space Reclaiming Plots Visualizer")
 
         # This is where the plot will be 
-        plot_frame = Frame(root, bg="pink")
-        plot_frame.grid(row=0, column=1, columnspan=3, rowspan=5, sticky="nsew", 
-            padx=(10,10), pady=(10,10))
+        plot_frame = Frame(self.root, bg="pink")
+        plot_frame.pack(side=RIGHT, fill=BOTH, expand=TRUE)
+
+        # This is the frame for the rest (with grid layout)
+        left_frame = Frame(self.root, bg = self.bg_color)
+        left_frame.place(relheight=1, width = 250)
+        # The grid layout
+        for i in range(6):
+            left_frame.rowconfigure(i, weight=1)
+        left_frame.columnconfigure(0, weight=1)
 
         # The button/filethingy used for importing
-        import_button = Button(root, text="Import", bg=self.bg_color, fg="white", 
+        import_button = Button(left_frame, text="Import", bg=self.bg_color, fg="white", 
             activebackground=self.second_color, activeforeground="white", 
             command=lambda:import_button_function(self))
-        import_button.grid(row=0, column=0, sticky="nsew", padx=(10,10), pady=(10,10))
+        import_button.grid(row=0, column=0, sticky="nsew", padx=(10,10), pady=(10,0))
+
+        self.file_label = Label(left_frame, text="No file selected yet", 
+            bg = self.bg_color, fg = "white", wraplength= 230)
+        self.file_label.grid(row=1, column=0, sticky = "w", padx=(10,0), pady=(0,10))
 
         # The change vis button
-        change_button = Button(root, text="Change visualization", bg=self.bg_color, 
+        change_button = Button(left_frame, text="Change visualization", bg=self.bg_color, 
             fg="white", activebackground=self.second_color, activeforeground="white", 
             command=lambda:change_vis_button_function(self))
-        change_button.grid(row=1, column=0, sticky="nsew", padx=(10,10), pady=(10,10))
+        change_button.grid(row=2, column=0, sticky="nsew", padx=(10,10), pady=(10,10))
 
         # The export button
-        export_button = Button(root, text="Export", bg=self.bg_color, fg="white", 
+        export_button = Button(left_frame, text="Export", bg=self.bg_color, fg="white", 
             activebackground=self.second_color, activeforeground="white",
             command=lambda:export_button_function(self))
-        export_button.grid(row=2, column=0, sticky="nsew", padx=(10,10), pady=(10,10))
+        export_button.grid(row=3, column=0, sticky="nsew", padx=(10,10), pady=(10,10))
 
         # The animation speed slider (ugly)
-        animation_speed = Scale(root, from_=0, to=100, orient=HORIZONTAL, bg=self.bg_color, 
+        animation_speed = Scale(left_frame, from_=0, to=100, orient=HORIZONTAL, bg=self.bg_color, 
             fg="white", label="Animation Speed", showvalue=0, troughcolor=self.second_color, 
             highlightcolor=self.second_color, bd=1, highlightthickness=0, 
             activebackground=self.second_color)
-        animation_speed.grid(row=3, column=0, sticky="ew", padx=(10,10), pady=(10,10))
+        animation_speed.grid(row=4, column=0, sticky="ew", padx=(10,10), pady=(10,10))
 
         # The information frame
-        information_frame = Frame(root, bg = self.second_color)
-        information_frame.grid(row=4, column=0, sticky="nsew", padx=(10,10), pady=(10,10))
+        information_frame = Frame(left_frame, bg = self.second_color)
+        information_frame.grid(row=5, column=0, sticky="nsew", padx=(10,10), pady=(10,10))
         for i in range(4):
             information_frame.rowconfigure(i, weight=1)
         
@@ -99,5 +104,5 @@ class GUI:
         detail_label.grid(row=3, column=0, sticky = "w", padx=(10,10), pady=(10,10))
 
         # Run the GUI
-        root.mainloop()
+        self.root.mainloop()
 
